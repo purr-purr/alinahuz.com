9c2acafb3a48365da207fe029027b1ea048468e0
Shaposhnikov Anton <56505654+purr-purr@users.noreply.github.com>
2022-09-07 22:50:56 +0100

    feature: implement styles for Selected Works

---
+++ src/helpers/types.ts
@@ -0,0 +1,10 @@
+export interface ISelectedWorks {
+  poster: string;
+  number: number;
+  reverseState: boolean;
+  title: string;
+  description: string;
+  implementation: string;
+  link: string;
+  screenShots: [{ image: string }];
+}
