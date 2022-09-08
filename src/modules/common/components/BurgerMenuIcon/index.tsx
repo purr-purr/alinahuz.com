053999d2c10a6144d8554427678f61424f57d2c7
Shaposhnikov Anton <56505654+purr-purr@users.noreply.github.com>
2022-09-08 15:53:15 +0100

    fix: delete spaces

--- src/modules/common/components/BurgerMenuIcon/index.tsx
+++ src/modules/common/components/BurgerMenuIcon/index.tsx
@@ -1,29 +1,27 @@
-import {FC, useState} from 'react';
+import { FC, useState } from 'react';
 import cn from 'classnames';
 
 import s from './BurgerMenuIcon.module.scss';
-import Navigation from "@modules/common/components/Navigation";
+import Navigation from '@modules/common/components/Navigation';
 
 const BurgerMenuIcon: FC<{
-    isBlackColor?: boolean;
-}> = ({isBlackColor = false}) => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  isBlackColor?: boolean;
+}> = ({ isBlackColor = false }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <>
-            <button
-                onClick={() => setMenuOpen(!menuOpen)}
-                className={cn(
-                    s.container,
-                    {[s[`container--black`]]: isBlackColor},
-                    {[s.openState]: menuOpen},
-                )}
-            />
-            {menuOpen && (
-                <Navigation/>
-            )}
-        </>
-    );
+  return (
+    <>
+      <button
+        onClick={() => setMenuOpen(!menuOpen)}
+        className={cn(
+          s.container,
+          { [s[`container--black`]]: isBlackColor },
+          { [s.openState]: menuOpen },
+        )}
+      />
+      {menuOpen && <Navigation />}
+    </>
+  );
 };
 
 export default BurgerMenuIcon;
--- src/modules/common/components/Navigation/index.tsx
+++ src/modules/common/components/Navigation/index.tsx
@@ -1,23 +1,22 @@
-import {FC} from 'react';
+import { FC } from 'react';
 
-import {menuLinks} from '@helpers/data';
+import { menuLinks } from '@helpers/data';
 import DecorativeLink from '@modules/common/components/DecorativeLink';
 
 import s from './Navigation.module.scss';
 
 const Navigation: FC<{
-    isBlackColor?: boolean;
-}> = ({isBlackColor = false}) => {
-    return (
-        <ul className={s.container}>
-            {menuLinks.map((item) => (
-                <li>
-                    <DecorativeLink text={item.title} href={item.link}
-                                    isBlackColorState/>
-                </li>
-            ))}
-        </ul>
-    );
+  isBlackColor?: boolean;
+}> = ({ isBlackColor = false }) => {
+  return (
+    <ul className={s.container}>
+      {menuLinks.map((item) => (
+        <li>
+          <DecorativeLink text={item.title} href={item.link} isBlackColorState />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Navigation;
